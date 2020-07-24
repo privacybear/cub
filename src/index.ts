21a1d012e0b9474558762de3bdccc1c3bd19e9bf
Michael55555 <michaelpeters5647@gmail.com>
2020-07-24 08:08:38 +0200

    Add GeolocationAPI

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,35 @@
 namespace Wrapper {
-
+  //#region BatteryAPI
   Object.defineProperty(navigator, "getBattery", {
     value: async () => new BatteryManager(),
     writable: false,
   });
+  //#endregion BatteryAPI
+
+  //#region GeolocationAPI
+  if ("geolocation" in navigator) {
+    let geolocationWatchersCount = 0;
+
+    Object.defineProperty(navigator.geolocation, "getCurrentPosition", {
+      value: (func) => func(new GeolocationPosition()),
+      writable: false,
+    });
+
+    Object.defineProperty(navigator.geolocation, "watchPosition", {
+      value: (func) => {
+        func(new GeolocationPosition());
+        setTimeout(() => new GeolocationPosition(), 1000);
+        return ++geolocationWatchersCount;
+      },
+      writable: false,
+    });
+
+    Object.defineProperty(navigator.geolocation, "clearWatch", {
+      value: () => {},
+      writable: false,
+    });
+  }
+  //#endregion GeolocationAPI
 
   console.log("Cub is loaded 🐾");
 }
