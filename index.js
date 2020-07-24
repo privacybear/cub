66abe8cd855ff086cc97daac9756296c85f22218
Michael55555 <michaelpeters5647@gmail.com>
2020-07-24 07:50:08 +0200

    Fake Geolocation API

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+console.log("Injected script loaded");
+
+//#region BatteryMock
 Object.defineProperty(navigator, "getBattery", {
   value: async () => new BatteryManager(),
   writable: false,
 });
 
-//#region Mocks
 class BatteryManager {
   constructor() {
     this.charging = true;
@@ -14,11 +16,52 @@ class BatteryManager {
     this.onchargingtimechange = null;
     this.ondischargingtimechange = null;
     this.onlevelchange = null;
-    this.addEventListener = () => {};
     console.log(
       window.location.host + " just tried to access your battery stats"
     );
   }
+  addEventListener() {}
 }
-//#endregion Mocks
-console.log("Injected script loaded");
+//#endregion BatteryMock
+//#region GeolocationMock
+if ("geolocation" in navigator) {
+  let geolocationWatchersCount = 0;
+
+  Object.defineProperty(navigator.geolocation, "getCurrentPosition", {
+    value: (func) => func(new GeolocationPosition()),
+    writable: false,
+  });
+
+  Object.defineProperty(navigator.geolocation, "watchPosition", {
+    value: (func) => {
+      func(new GeolocationPosition());
+      setTimeout(() => new GeolocationPosition(), 1000);
+      return ++geolocationWatchersCount;
+    },
+    writable: false,
+  });
+
+  Object.defineProperty(navigator.geolocation, "clearWatch", {
+    value: () => {},
+    writable: false,
+  });
+
+  class GeolocationPosition {
+    constructor() {
+      this.coords = {
+        accuracy: 269309,
+        altitude: null,
+        altitudeAccuracy: null,
+        heading: null,
+        latitude: 51.085853,
+        longitude: 10.377857,
+        speed: null,
+      };
+      this.timestamp = Date.now();
+      console.log(
+        window.location.host + " just tried to access your geolocation data"
+      );
+    }
+  }
+}
+//#endregion GeolocationMock
